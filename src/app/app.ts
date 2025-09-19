bf9e0fc2c864f969853bf92b9a9941111a815052
deji-technify <i.ayodeji@tajnetworkafrica.com>
2025-09-19 22:29:33 +0100

    Added contact section with social media icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { About } from './components/about/about';
 import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, About, Projects],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, About, Projects, Contact],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
